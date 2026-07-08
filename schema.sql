dbc13092ae436a29c2d1ceb680cb2fc7584872a2
loickcherimont <loickcherimont@gmail.com>
2026-07-08 20:46:57 +0200

    feat: add relationship tables for menu and client

--- schema.sql
+++ schema.sql
@@ -60,3 +60,28 @@ CREATE TABLE IF NOT EXISTS ingredient_focaccia (
         FOREIGN KEY (id_focaccia) REFERENCES focaccia (id)
         ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS menu_boisson (
+    id_menu INT UNSIGNED,
+    id_boisson INT UNSIGNED,
+    PRIMARY KEY (id_menu, id_boisson),
+        CONSTRAINT fk_menu
+        FOREIGN KEY (id_menu) REFERENCES menu (id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_boisson
+        FOREIGN KEY (id_boisson) REFERENCES boisson (id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS client_menu (
+    id_client INT UNSIGNED,
+    id_menu INT UNSIGNED,
+    date_achat DATE NOT NULL,
+    PRIMARY KEY (id_client, id_menu, date_achat),
+        CONSTRAINT fk_client
+        FOREIGN KEY (id_client) REFERENCES client (id)
+        ON DELETE RESTRICT,
+    CONSTRAINT fk_menu_c
+        FOREIGN KEY (id_menu) REFERENCES menu (id)
+        ON DELETE CASCADE
+);
